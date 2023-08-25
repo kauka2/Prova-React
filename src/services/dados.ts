c0c7bbe8787fe354a091736ae6e51810be317306
kauka2 <barbosak2526@gmail.com>
2023-08-25 10:32:27 -0300

    Prova

---
+++ src/services/dados.ts
@@ -0,0 +1,23 @@
+export const dados = [
+  {
+    id: 1,
+    mensagem: "Culinária"
+  },
+  {
+    id: 2,
+    mensagem: "Culinária"
+  },
+  {
+    id: 3,
+    mensagem: "Culinária"
+  },
+  {
+    id: 4,
+    mensagem: "Culinária"
+  }
+  ,
+  {
+    id: 5,
+    mensagem: "Culinária"
+  },
+]
